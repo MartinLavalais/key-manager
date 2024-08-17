39bbeba71f943c679f8466ef1584c874a97c48ab
MartinLavalais <martin.lavalais2022@gmail.com>
2024-08-17 23:19:45 +0200

    Start of the index page

---
+++ public/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/navbar.css">
+    <link rel="stylesheet" href="/css/index.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="/css/button.css">
+    <title>Key Manager Service</title>
+</head>
+<body>
+    <nav class="navbar">
+        <div class="navbar-part-left">
+            <a class="navbar-logo-link" href="/">
+                <img class="navbar-logo" alt="Atlas-Eternal Logo" src="/img/logo_atlas.png">
+            </a>
+        </div>
+        <div class="navbar-part-right">
+            <svg class="navbar-icon bi bi-list" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
+            </svg>
+        </div>
+    </nav>
+    <main>
+        <div class="section-title">
+            <div class="container-title">
+                <div class="title" id="name">Atlas</div><br class="title-br">
+                <div class="title"> 
+                    <span>protect your data</span>
+                </div>
+            </div>
+            <div class="container-button">
+                <section>
+                    <div class="register-button" data-download="S'inscrire"></div>
+                </section>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
